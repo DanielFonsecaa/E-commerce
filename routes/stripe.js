1413e9aadde590225f46334a7e2fdf393d94fd9d
daniel <danieldafonseca@sapo.pt>
2024-09-28 14:27:45 +0100

    adding stripe route

---
+++ routes/stripe.js
@@ -0,0 +1,23 @@
+import express from "express";
+import stripe from "stripe";
+
+const router = express.Router();
+
+router.post("/payment", async (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
+
+export default router;
