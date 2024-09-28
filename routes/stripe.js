e866ff90714ec4315889ad28264fd9e10261cefa
daniel <danieldafonseca@sapo.pt>
2024-09-28 14:36:30 +0100

    fixing stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,23 +1,18 @@
 import express from "express";
-import stripe from "stripe";
+import Stripe from "stripe";
 
 const router = express.Router();
-
+const stripe = new Stripe(process.env.SRIPE_KEY);
 router.post("/payment", async (req, res) => {
-  stripe.charges.create(
-    {
+  try {
+    const stripeRes = await stripe.charges.create({
       source: req.body.tokenId,
       amount: req.body.amount,
       currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).json(stripeErr);
-      } else {
-        res.status(200).json(stripeRes);
-      }
-    }
-  );
+    });
+    res.status(200).json(stripeRes);
+  } catch (stripeErr) {
+    res.status(500).json(stripeErr);
+  }
 });
-
 export default router;
